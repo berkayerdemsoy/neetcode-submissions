4df12bce0734e9e7d26f52bcd60ced53bd981073
Berkay Erdemsoy <114877481+berkayerdemsoy@users.noreply.github.com>
2026-04-16 15:12:41 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i]) + 1 );
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+        return false;
+        
+    }
+}
